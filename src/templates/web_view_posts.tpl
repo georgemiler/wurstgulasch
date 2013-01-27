b077d3accf0c62cee61c6b7769dcff4cfa7b89b0
Johannes Fürmann <waaaaargh@quantentunnel.de>
2013-01-27 03:40:13 +0100

    now saves images passed as url. views.py needs refactoring. tomorrow :>

--- src/templates/web_view_posts.tpl
+++ src/templates/web_view_posts.tpl
@@ -4,10 +4,11 @@
 <body>
 <ul>
 {% for post in posts %}
-<item> {{post.post_id}}: <br>
+<li> {{post.post_id}}: <br>
 {% if post.content_type == "image" %}
 <img src="{{post.content_string}}"><br>
 {% endif %}
+{{ post.description }} </li>
 {%endfor%}
 </ul>
 </body>
--- src/views.py
+++ src/views.py
@@ -1,12 +1,14 @@
 import os
 import random
 import time
+from hashlib import md5
 
 from werkzeug.wrappers import Response
 
 from jinja2 import Environment, FileSystemLoader
 
 from model import post
+from config import Configuration
 
 def render_template(template_name, **context):
     extensions = context.pop('extensions', [])
@@ -20,6 +22,20 @@ def render_template(template_name, **context):
 
     #jinja_env.update_template_context(context)
     return jinja_env.get_template(template_name).render(context)
+
+def generate_thumbnail(filename):
+    from PIL import Image
+    im = Image.open(os.path.join('assets', filename))
+                    
+    # guttenberg'd from:
+    #   http://jargonsummary.wordpress.com/2011/01/08/how-to-resize-images-with-python/
+    # TODO: Improve algorithm
+    basewidth = 300
+    wpercent = (basewidth / float(im.size[0]))
+    hsize = int((float(im.size[1]) * float(wpercent)))
+    im = im.resize((basewidth, hsize), Image.ANTIALIAS)
+    im.save(os.path.join('assets', 'thumb_'+filename))
+    
 
 def json_since(request, timestamp):
     posts = post.get_posts(since=timestamp)
@@ -36,47 +52,62 @@ def web_view_posts(request, page=1, posts_per_page=30):
     out = render_template(template_name="web_view_posts.tpl", posts=posts)
     return Response(out, mimetype="text/html")        
 
+# TODO: Refactor
 def web_insert_post(request):
     if request.method == "POST":
-        
-        # TODO: Verify contents
-    
-        if request.form['content_string'] == None or request.form['content_string'] == "":
+        url = Configuration().base_url+"assets/"
+        if request.form['content_string'] == "":
             # either we have a file upload or the request is incomplete!
             uploaded = request.files.get('file')
             if uploaded:
                 mimetype = uploaded.content_type
                 if mimetype.split('/')[0] == "image" and mimetype.split('/')[1] in ['jpeg', 'png', 'gif', 'tiff']:
-                    from hashlib import md5
                     hash = md5(uploaded.read())
                     uploaded.seek(0)
                     filename = str(hash.hexdigest()) + "." + mimetype.split('/')[1]
                     uploaded.save(os.path.join('assets', filename))
-                    uploaded.seek(0)
-                    from PIL import Image
-                    im = Image.open(os.path.join('assets', filename))
-                    
-                    # guttenberg'd from:
-                    #   http://jargonsummary.wordpress.com/2011/01/08/how-to-resize-images-with-python/
-                    # TODO: Improve algorithm
-                    basewidth = 300
-                    wpercent = (basewidth / float(im.size[0]))
-                    hsize = int((float(im.size[1]) * float(wpercent)))
-                    im = im.resize((basewidth, hsize), Image.ANTIALIAS)
-                    im.save(os.path.join('assets', 'thumb_'+filename))
-                    
+                    generate_thumbnail(filename)
+                    url += filename
                     # TODO implement size check
                 else:
                     raise Exception("Invalid File uploaded!")
             else:
                 raise Exception("Incomplete request!")
+        else:
+            if request.form['content_type'] == "image":
+                # get Image from URL
+                from urllib2 import urlopen
+                try:
+                    image = urlopen(request.form['content_string'])
+                    if image.info().getmaintype() != "image":
+                        raise Exception("URL was not an image!")
+                    if image.info().getsubtype() not in ['jpeg', 'png', 'gif', 'tiff']:
+                        raise Exception("Image type not accepted! ("+image.info().getsubtype()+")")
+                    
+                    raw = image.read()
+                    hash = md5(raw)
+                    filename = str(hash.hexdigest())+"."+image.info().getsubtype()
+                    f = open(os.path.join('assets', filename), 'w')
+                    f.write(raw)
+                    f.close()                   
+
+                    generate_thumbnail(filename)
+                    url += filename
+                except Exception,e:
+                    raise Exception("Fehler beim Spiegeln der URL! "+str(e))
+                
+            
+        if request.form['content_type'] == "image":
+            content_string = url
+        else:
+            content_string = request.form['content_string']
 
         tmp = post.post(
             post_id=random.randint(1,2**32),
             timestamp=int(time.time()),
             origin="http://dev.img.sft.mx/",
             content_type=request.form['content_type'],
-            content_string=request.form['content_string'],
+            content_string=content_string,
             source=request.form['source'],
             tags=None,
             description=request.form['description'],
